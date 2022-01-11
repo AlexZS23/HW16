3683fa93f28be44a105e8f05557743e2360f04ea
zatoliuk <zatoliuk@cleverstaff.net>
2022-01-11 11:07:04 +0200

    empty files

---
+++ src/test/java/HomeWork16/FirstTest.java
@@ -0,0 +1,10 @@
+package HomeWork16;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+public class FirstTest {
+
+}
